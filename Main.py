1eb9a477ef1021066c10364f095bad7fc4b27578
BillNikolo <vasileios.nikolopoulos.001@student.uni.lu>
2021-10-12 13:07:30 +0200

    Fixes/ New Classes/ PPM

--- Main.py
+++ Main.py
@@ -1,19 +1,17 @@
 from math import sqrt
+import array
 
-def quadratic(a,b,c, x0, x1):
+def quadratic(a, b, c):
   discriminant = b ** 2 - 4 * a * c
   if discriminant < 0:
-    return False
+    return None
   elif discriminant == 0:
-    x1 = - 0,5 * b / a
-    x2 = x1
-    return True
+    x0 = - 0, 5 * b / a
+    return x0
   else:
-    x1 = -0.5 * (b + sqrt(discriminant))/ 2 * a
-    x2 = -0.5 * (b - sqrt(discriminant))/ 2 * a
-    if x1 > x2:
-      x1, x2 = x2, x1
-    return True
+    x1 = -0.5 * (b + sqrt(discriminant))/2 * a
+    x2 = -0.5 * (b - sqrt(discriminant))/2 * a
+    return min(x1, x2)
 
 class Vector:
 
@@ -64,30 +62,92 @@ class Ray:
 
   def __init__(self, origin, direction):
     self.orig = origin
-    self.dir = direction.normalize()
+    self.dir = direction  # .normalize()
 
 
 class Sphere:
 
-  def __init__(self, center, radius):
+  def __init__(self, center: Vector, radius):
     self.c = center
     self.r = radius
 
   def intersects(self, ray):
-    t0 = 0
-    t1 = 0
     dif = ray.orig - self.c
     a = ray.dir.dot_p(ray.dir)
     b = 2 * ray.dir.dot_p(dif)
     c = dif.dot_p(dif) - self.r
-    if not(quadratic(a, b, c, t0, t1)):
-      return True
+    point_of_intersect = quadratic(a, b, c)
+    return point_of_intersect
+
+
+class Camera:
+  def __init__(self, width, height, focal_length, origin: Vector):
+    self.width = width
+    self.height = height
+    self.focal_length = focal_length
+    self.origin = origin
+
+  def generate_ray(self):
+    for y in range(self.height):
+      for x in range(self.width):
+        pixel_pos = Vector(x-(self.width/2), y - (self.height/2), self.focal_length)
+        direction = pixel_pos.sub(self.origin)
+        yield Ray(self.origin, direction), x, y
+
+class Image:
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+    self.max_val = 255
+    self.pixels = [[None for _ in range(width)] for _ in range(height)]
+    ppm_header = f'P3 {self.width} {self.height} {self.max_val}\n'
+
+  def set_pixel(self, x, y, elem):
+    self.pixels[y][x] = elem
+
+  def write_ppm(self, img_file):
+    def to_byte(c):
+      return round(max(min(c * 255, 255), 0))
+    for row in self.pixels:
+      for color in row:
+        img_file.write(
+          "{} {} {} ".format(
+            to_byte(color.x), to_byte(color.y), to_byte(color.z)
+          )
+        )
+      img_file.write("\n")
+
+#class Color(Vector):
 
 
 class Scene:
 
-  def __init__(self, camera, object, width, height):
+  def __init__(self, camera, object_list: list, width, height):
     self.cam = camera
-    self.ob = object
+    self.ob = object_list
     self.w = width
     self.h = height
+
+  def render(self, camera):
+    infinity = 10 ** 10
+    for ray, x, y in camera.generate_ray():
+      min_intersection_dist = infinity
+      nearest_object = None
+      for object in self.object_list:
+        intersection_distance = object.intersects(ray)
+        if intersection_distance < min_intersection_dist:
+          min_intersection_dist = intersection_distance
+          nearest_object = object
+
+    if nearest_object:
+      pass
+    # Draw the object
+
+
+
+if __name__ == '__main__':
+  ppm_header = f'P3 {500} {600} {255}\n'
+  image = array.array('B', [0, 0, 255] * 500 * 600)
+  with open('base.ppm', 'wb') as f:
+    f.write(bytearray(ppm_header, 'ascii'))
+    image.tofile(f)
