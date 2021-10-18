75a62ed77d62bd0bf141ef97436228d5a79ab63c
BillNikolo <vasileios.nikolopoulos.001@student.uni.lu>
2021-10-18 22:49:25 +0200

    PPM Ready, Rendering Class, small adjustments

--- Main.py
+++ Main.py
@@ -1,6 +1,18 @@
 from math import sqrt
 import array
 
+# Colors
+DARK_ORCHID = [191, 62, 255]
+SKY_BLUE = [135, 206, 255]
+ROYAL_BLUE = [72, 118, 255]
+RED = [238, 44, 44]
+YELLOW = [255, 255, 0]
+EMERALD_GREEN = [0, 201, 87]
+BLACK = [0, 0, 0]
+WHITE = [255, 255, 255]
+
+INFINITY = 10 ** 10
+
 def quadratic(a, b, c):
   discriminant = b ** 2 - 4 * a * c
   if discriminant < 0:
@@ -11,7 +23,8 @@ def quadratic(a, b, c):
   else:
     x1 = -0.5 * (b + sqrt(discriminant))/2 * a
     x2 = -0.5 * (b - sqrt(discriminant))/2 * a
-    return min(x1, x2)
+    sol = [x1, x2]
+    return min([i for i in sol if i > 0])
 
 class Vector:
 
@@ -67,87 +80,113 @@ class Ray:
 
 class Sphere:
 
-  def __init__(self, center: Vector, radius):
+  def __init__(self, center: Vector, radius, color):
     self.c = center
     self.r = radius
+    self.col = color
 
   def intersects(self, ray):
     dif = ray.orig - self.c
     a = ray.dir.dot_p(ray.dir)
     b = 2 * ray.dir.dot_p(dif)
     c = dif.dot_p(dif) - self.r
-    point_of_intersect = quadratic(a, b, c)
-    return point_of_intersect
+    length = quadratic(a, b, c)
+    return length
 
 
 class Camera:
-  def __init__(self, width, height, focal_length, origin: Vector):
+  def __init__(self, width, height, focal_distance, origin: Vector):
     self.width = width
     self.height = height
-    self.focal_length = focal_length
+    self.focal_dis = focal_distance
     self.origin = origin
 
   def generate_ray(self):
     for y in range(self.height):
       for x in range(self.width):
-        pixel_pos = Vector(x-(self.width/2), y - (self.height/2), self.focal_length)
+        pixel_pos = Vector(x-(self.width/2), y - (self.height/2), self.focal_dis)
         direction = pixel_pos.sub(self.origin)
         yield Ray(self.origin, direction), x, y
 
 class Image:
-  def __init__(self, width, height):
+  def __init__(self, width: int, height: int):
     self.width = width
     self.height = height
     self.max_val = 255
-    self.pixels = [[None for _ in range(width)] for _ in range(height)]
-    ppm_header = f'P3 {self.width} {self.height} {self.max_val}\n'
+    self.ppm_header = f'P6 {self.width} {self.height} {self.max_val}\n'
+    self.image = array.array('B', BLACK * self.width * self.height)
 
-  def set_pixel(self, x, y, elem):
-    self.pixels[y][x] = elem
+  def set_pixel_color(self, x, y, color):
+    index = 3 * (y * self.width + x)
+    self.image[index] = color[0]
+    self.image[index + 1] = color[1]
+    self.image[index + 2] = color[2]
 
-  def write_ppm(self, img_file):
-    def to_byte(c):
-      return round(max(min(c * 255, 255), 0))
-    for row in self.pixels:
-      for color in row:
-        img_file.write(
-          "{} {} {} ".format(
-            to_byte(color.x), to_byte(color.y), to_byte(color.z)
-          )
-        )
-      img_file.write("\n")
+  def export_ppm(self):
+    with open('base_Image.ppm', 'wb') as f:
+      f.write(bytearray(self.ppm_header, "ascii"))
+      self.image.tofile(f)
 
-#class Color(Vector):
+
+class Engine:
+
+  def render(self, scene):
+    width = scene.width
+    height = scene.height
+    aspect_ratio = width / height
+    x0 = -1.0
+    x1 = +1.0
+    x_step = (x1 - x0) / (width - 1)
+    y0 = -1.0 / aspect_ratio
+    y1 = +1.0 / aspect_ratio
+    y_step = (y1 - y0) / (height - 1)
+
+    camera = scene.camera
+    panel = Image(width, height)
+
+    for j in range(height):
+      y = y0 + j * y_step
+      for i in range(width):
+        x = x0 + i * x_step
+        ray = Ray(camera, Vector(x, y, 0).sub(camera))
+        panel.set_pixel_color(i, j, self.ray_trace(ray, scene))
+
+  def ray_trace(self, ray, scene):
+    color = BLACK
+    dist_hit, obj_hit = self.nearest_object(scene, ray)
+    if obj_hit is None:
+      return color
+    else:
+      return obj_hit.col
+
+  def nearest_object(self, scene, ray):
+    min_distance = None
+    object_hit = None
+    for ob in scene.ob:
+      distance = scene.ob.interects(ray)
+      if distance is not None:
+        min_distance = distance
+        object_hit = ob
+    return min_distance, object_hit
 
 
 class Scene:
 
-  def __init__(self, camera, object_list: list, width, height):
+  def __init__(self, camera, object_list: list, width: int, height: int):
     self.cam = camera
     self.ob = object_list
     self.w = width
     self.h = height
 
-  def render(self, camera):
-    infinity = 10 ** 10
-    for ray, x, y in camera.generate_ray():
-      min_intersection_dist = infinity
-      nearest_object = None
-      for object in self.object_list:
-        intersection_distance = object.intersects(ray)
-        if intersection_distance < min_intersection_dist:
-          min_intersection_dist = intersection_distance
-          nearest_object = object
 
-    if nearest_object:
-      pass
-    # Draw the object
-
+def main():
+  pass
+  # objects = [Sphere(Vector(0, 0, 0))]
+  # scene = Scene(Vector(0, 0, 0), objects, 1920, 1080)
 
 
 if __name__ == '__main__':
-  ppm_header = f'P3 {500} {600} {255}\n'
-  image = array.array('B', [0, 0, 255] * 500 * 600)
-  with open('base.ppm', 'wb') as f:
-    f.write(bytearray(ppm_header, 'ascii'))
-    image.tofile(f)
+  # main()
+  image = Image(1920, 1080)
+  image.set_pixel_color(300, 300, ROYAL_BLUE)
+  image.export_ppm()
